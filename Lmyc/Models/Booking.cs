4f1e48d859bba530b1898b2ff5eaac364e82d8fc
Jason <jasonccmfry@gmail.com>
2018-04-10 14:10:40 -0700

    Seeding more users

--- Lmyc/Models/Booking.cs
+++ Lmyc/Models/Booking.cs
@@ -14,13 +14,11 @@ namespace Lmyc.Models
         public int BookingId { get; set; }
 
         [Required(ErrorMessage = "Start Date Time is required.")]
-        [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}", ApplyFormatInEditMode = true)]
         [DisplayName("From Date")]
         public DateTime StartDateTime { get; set; }
 
         [Required(ErrorMessage = "End Date Time is required.")]
-        [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}", ApplyFormatInEditMode = true)]
         [DisplayName("To Date")]
         public DateTime EndDateTime { get; set; }
