250f71005f5788a32e3b445ed8c7c0d023f94b3d
Jason <jasonccmfry@gmail.com>
2018-04-11 15:37:20 -0700

    Add member api

---
+++ Lmyc/Controllers/API/MembersAPIController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Lmyc.Data;
+using Lmyc.Models;
+using Lmyc.Models.UserViewModels;
+using Microsoft.AspNetCore.Identity;
+
+namespace Lmyc.Controllers.API
+{
+    [Produces("application/json")]
+    [Route("api/MembersAPI")]
+    public class MembersAPIController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public MembersAPIController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        // GET: api/MembersAPI
+        [HttpGet]
+        public IEnumerable<UserViewModel> GetMembers()
+        {
+            var users = _userManager.Users;
+
+            var models = new List<UserViewModel>();
+            
+            foreach (var u in users)
+            {
+                var model = new UserViewModel
+                {
+                    UserId = u.Id,
+                    FirstName = u.FirstName,
+                    LastName = u.LastName,
+                    Role = string.Join(", ", _userManager.GetRolesAsync(u).Result)
+                };
+
+                models.Add(model);
+            }
+
+            return models;
+        }
+
+        // GET: api/MembersAPI/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetMember([FromRoute] string id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var applicationUser = await _userManager.Users.SingleOrDefaultAsync(m => m.Id == id);
+
+            if (applicationUser == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(applicationUser);
+        }
+    }
+}
---
+++ Lmyc/Models/UserViewModels/UserViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lmyc.Models.UserViewModels
+{
+    public class UserViewModel
+    {
+        public string UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Role { get; set; }
+    }
+}
